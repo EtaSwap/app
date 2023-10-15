6ace50f8e1a155a74d9f395c09734b4ff1d17e3c
Danylo Moshenskyi <moshenskyi.danylo@gmail.com>
2023-10-15 23:51:08 +0200

    fix: tokens route

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,19 +4,19 @@ function Social() {
   return (
     <>
       <a className="social__link" href="https://twitter.com/eta_swap" target="_blank" title="Twitter">
-        <img className="social__image" src="./icon_twitter.svg" alt="Twitter" />
+        <img className="social__image" src="/icon_twitter.svg" alt="Twitter" />
       </a>
       <a className="social__link" href="https://t.me/etaswap" target="_blank" title="Telegram">
-        <img className="social__image" src="./icon_telegram.svg" alt="Telegram" />
+        <img className="social__image" src="/icon_telegram.svg" alt="Telegram" />
       </a>
       <a className="social__link" href="https://www.reddit.com/r/etaswap/" target="_blank" title="Reddit">
-        <img className="social__image" src="./icon_reddit.svg" alt="Reddit" />
+        <img className="social__image" src="/icon_reddit.svg" alt="Reddit" />
       </a>
       <a className="social__link" href="https://discord.gg/gZshGtXW2" target="_blank" title="Discord">
-        <img className="social__image" src="./icon_discord.svg" alt="Discord" />
+        <img className="social__image" src="/icon_discord.svg" alt="Discord" />
       </a>
       <a className="social__link" href="https://github.com/EtaSwap" target="_blank" title="GitHub">
-        <img className="social__image" src="./icon_github.svg" alt="GitHub" />
+        <img className="social__image" src="/icon_github.svg" alt="GitHub" />
       </a>
     </>
   )
