337e2ca8a61c84783e9396edcfc3c513d8204749
Danylo Moshenskyi <moshenskyi.danylo@gmail.com>
2023-11-14 15:59:20 +0100

    hotfix: gas limit for token->hbar increase

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const GAS_LIMITS = {
     SaucerSwap: {
         exactTokenToToken: 900000, //877969    875079
         exactHBARToToken: 260000, //221207     203366
-        exactTokenToHBAR: 1670000, //1629306   1623679     1336000
+        exactTokenToHBAR: 1687000, //1629306   1623679     1336000
         tokenToExactToken: 920000, //894071    891182
         HBARToExactToken: 260000, //211040     218135      208000
         tokenToExactHBAR: 1690000, //1645353   1639941     1352000
@@ -15,7 +15,7 @@ export const GAS_LIMITS = {
     Pangolin: {
         exactTokenToToken: 900000,
         exactHBARToToken: 260000,
-        exactTokenToHBAR: 1670000,
+        exactTokenToHBAR: 1687000,
         tokenToExactToken: 920000,
         HBARToExactToken: 260000,
         tokenToExactHBAR: 1690000,
